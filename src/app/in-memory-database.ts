72910669a7c3fa23b6ae983b2ff630504ad0e7b5
Admin Account GustavoL <dev.GustavoL@sefaz.rs.gov.br>
2019-04-04 09:03:26 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,17 @@
+import {InMemoryDbService} from 'angular-in-memory-web-api';
+
+export class InMemoryDatabase implements InMemoryDbService {
+
+  createDb() {
+    const categories = [
+      { id: 1, name: 'Moradia', description: 'Pagamentos contas da casa'},
+      { id: 2, name: 'Saúde', description: 'Plano de saúde e remedios'},
+      { id: 3, name: 'Lazer', description: 'cinemas, praias, parques, etc'},
+      { id: 4, name: 'Salários', description: 'Recebimento de salarios'},
+      { id: 5, name: 'Freelas', description: 'Trabalhos como freelancer'}
+    ];
+
+    return { categories }
+  }
+
+}
